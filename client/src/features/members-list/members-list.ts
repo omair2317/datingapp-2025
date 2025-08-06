4106c9e6564072eebef616c9fac258b43cdbaec6
omair2317 <mohammed.omar2317@gmail.com>
2025-08-06 17:02:52 +0530

    End of section 9

--- client/src/features/members-list/members-list.ts
+++ client/src/features/members-list/members-list.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MemberService } from '../../core/services/member-service';
+import { Observable } from 'rxjs';
+import { Member } from '../../types/member';
+import { AsyncPipe } from '@angular/common';
+import { MemberCard } from "../members/member-card/member-card";
 
 @Component({
   selector: 'app-members-list',
-  imports: [],
+  imports: [AsyncPipe, MemberCard],
   templateUrl: './members-list.html',
   styleUrl: './members-list.css'
 })
 export class MembersList {
+  private memberService = inject(MemberService);
+  protected members$: Observable<Member[]>;
 
+  constructor() {
+    this.members$ = this.memberService.getMembers();
+  }
 }
